373aa980e3e0ec0236347de29491802bfc1aa669
criptych <criptych@gmail.com>
2015-10-09 16:13:54 -0400

    Add Transform3D class

---
+++ include/SFML/Graphics/Transform3D.hpp
@@ -0,0 +1,311 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_TRANSFORM3D_HPP
+#define SFML_TRANSFORM3D_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics/Export.hpp>
+#include <SFML/Graphics/TransformBase.hpp>
+#include <SFML/System/Vector3.hpp>
+
+
+namespace sf
+{
+////////////////////////////////////////////////////////////
+/// \brief Define a 4x4 transform matrix
+///
+////////////////////////////////////////////////////////////
+class SFML_GRAPHICS_API Transform3D : public TransformBase
+{
+public:
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Default constructor
+    ///
+    /// Creates an identity transform (a transform that does nothing).
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a transform from a 4x4 matrix
+    ///
+    /// \param a00 Element (0, 0) of the matrix
+    /// \param a01 Element (0, 1) of the matrix
+    /// \param a02 Element (0, 2) of the matrix
+    /// \param a03 Element (0, 3) of the matrix
+    /// \param a10 Element (1, 0) of the matrix
+    /// \param a11 Element (1, 1) of the matrix
+    /// \param a12 Element (1, 2) of the matrix
+    /// \param a13 Element (1, 3) of the matrix
+    /// \param a20 Element (2, 0) of the matrix
+    /// \param a21 Element (2, 1) of the matrix
+    /// \param a22 Element (2, 2) of the matrix
+    /// \param a23 Element (2, 3) of the matrix
+    /// \param a30 Element (3, 0) of the matrix
+    /// \param a31 Element (3, 1) of the matrix
+    /// \param a32 Element (3, 2) of the matrix
+    /// \param a33 Element (3, 3) of the matrix
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D(float a00, float a01, float a02, float a03,
+                float a10, float a11, float a12, float a13,
+                float a20, float a21, float a22, float a23,
+                float a30, float a31, float a32, float a33);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a transform from another
+    ///
+    /// Copies the matrix from another transform (2D or 3D).
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D(const TransformBase &transform);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Return the inverse of the transform
+    ///
+    /// If the inverse cannot be computed, an identity transform
+    /// is returned.
+    ///
+    /// \return A new transform which is the inverse of self
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D getInverse() const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with another one
+    ///
+    /// The result is a transform that is equivalent to applying
+    /// *this followed by \a transform. Mathematically, it is
+    /// equivalent to a matrix multiplication.
+    ///
+    /// \param transform Transform to combine with this transform
+    ///
+    /// \return Reference to *this
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& combine(const TransformBase& transform);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a translation
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.translate(sf::Vector3f(100, 200)).rotate(45);
+    /// \endcode
+    ///
+    /// \param offset Translation offset to apply
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see rotate, scale
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& translate(const Vector3f& offset);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a rotation
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.rotate(90).translate(50, 20);
+    /// \endcode
+    ///
+    /// \param angle Rotation angle, in degrees
+    /// \param axis Rotation axis
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, scale
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& rotate(float angle, const Vector3f &axis);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a rotation
+    ///
+    /// The center of rotation is provided for convenience as a second
+    /// argument, so that you can build rotations around arbitrary points
+    /// more easily (and efficiently) than the usual
+    /// translate(-center).rotate(angle).translate(center).
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.rotate(90, sf::Vector3f(8, 3)).translate(sf::Vector3f(50, 20));
+    /// \endcode
+    ///
+    /// \param angle Rotation angle, in degrees
+    /// \param axis Rotation axis
+    /// \param center Center of rotation
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, scale
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& rotate(float angle, const Vector3f &axis, const Vector3f& center);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a scaling
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.scale(2, 1).rotate(45);
+    /// \endcode
+    ///
+    /// \param factor Scaling factor
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, rotate
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& scale(float factor);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a scaling
+    ///
+    /// The center of scaling is provided for convenience as a second
+    /// argument, so that you can build scaling around arbitrary points
+    /// more easily (and efficiently) than the usual
+    /// translate(-center).scale(factors).translate(center).
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.scale(sf::Vector3f(2, 1), sf::Vector3f(8, 3)).rotate(45);
+    /// \endcode
+    ///
+    /// \param factor Scaling factor
+    /// \param center Center of scaling
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, rotate
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& scale(float factor, const Vector3f& center);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a scaling
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.scale(sf::Vector3f(2, 1)).rotate(45);
+    /// \endcode
+    ///
+    /// \param factors Scaling factors
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, rotate
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& scale(const Vector3f& factors);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Combine the current transform with a scaling
+    ///
+    /// The center of scaling is provided for convenience as a second
+    /// argument, so that you can build scaling around arbitrary points
+    /// more easily (and efficiently) than the usual
+    /// translate(-center).scale(factors).translate(center).
+    ///
+    /// This function returns a reference to *this, so that calls
+    /// can be chained.
+    /// \code
+    /// sf::Transform3D transform;
+    /// transform.scale(sf::Vector3f(2, 1), sf::Vector3f(8, 3)).rotate(45);
+    /// \endcode
+    ///
+    /// \param factors Scaling factors
+    /// \param center Center of scaling
+    ///
+    /// \return Reference to *this
+    ///
+    /// \see translate, rotate
+    ///
+    ////////////////////////////////////////////////////////////
+    Transform3D& scale(const Vector3f& factors, const Vector3f& center);
+};
+
+} // namespace sf
+
+
+#endif // SFML_TRANSFORM3D_HPP
+
+
+////////////////////////////////////////////////////////////
+/// \class sf::Transform3D
+/// \ingroup graphics
+///
+/// A sf::Transform3D specifies how to translate, rotate, scale,
+/// shear, project, whatever things. In mathematical terms, it defines
+/// how to transform a coordinate system into another.
+///
+/// For example, if you apply a rotation transform to a sprite, the
+/// result will be a rotated sprite. And anything that is transformed
+/// by this rotation transform will be rotated the same way, according
+/// to its initial position.
+///
+/// Transforms are typically used for drawing. But they can also be
+/// used for any computation that requires to transform points between
+/// the local and global coordinate systems of an entity (like collision
+/// detection).
+///
+/// Example:
+/// \code
+/// // define a translation transform
+/// sf::Transform3D translation;
+/// translation.translate(20, 50);
+///
+/// // define a rotation transform
+/// sf::Transform3D rotation;
+/// rotation.rotate(45);
+///
+/// // combine them
+/// sf::Transform3D transform = translation * rotation;
+///
+/// // use the result to transform stuff...
+/// sf::Vector3f point = transform.transformPoint(10, 20);
+/// sf::FloatRect rect = transform.transformRect(sf::FloatRect(0, 0, 10, 100));
+/// \endcode
+///
+/// \see sf::Transformable, sf::RenderStates
+///
+////////////////////////////////////////////////////////////
---
+++ src/SFML/Graphics/Transform3D.cpp
@@ -0,0 +1,244 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics/Transform3D.hpp>
+#include <cmath>
+
+
+namespace sf
+{
+////////////////////////////////////////////////////////////
+Transform3D::Transform3D() :
+TransformBase()
+{
+
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D::Transform3D(const TransformBase &transform) :
+TransformBase(transform)
+{
+
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D::Transform3D(float a00, float a01, float a02, float a03,
+                         float a10, float a11, float a12, float a13,
+                         float a20, float a21, float a22, float a23,
+                         float a30, float a31, float a32, float a33) :
+TransformBase(a00, a01, a02, a03,
+              a10, a11, a12, a13,
+              a20, a21, a22, a23,
+              a30, a31, a32, a33)
+{
+
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D Transform3D::getInverse() const
+{
+    const float *a = getMatrix();
+    float b[16];
+
+    b[0]  =  a[5] * a[10] * a[15] - a[5]  * a[11] * a[14] - a[9]  * a[6] * a[15] +
+             a[9] * a[7]  * a[14] + a[13] * a[6]  * a[11] - a[13] * a[7] * a[10];
+    b[4]  = -a[4] * a[10] * a[15] + a[4]  * a[11] * a[14] + a[8]  * a[6] * a[15] -
+             a[8] * a[7]  * a[14] - a[12] * a[6]  * a[11] + a[12] * a[7] * a[10];
+    b[8]  =  a[4] * a[9]  * a[15] - a[4]  * a[11] * a[13] - a[8]  * a[5] * a[15] +
+             a[8] * a[7]  * a[13] + a[12] * a[5]  * a[11] - a[12] * a[7] * a[9];
+    b[12] = -a[4] * a[9]  * a[14] + a[4]  * a[10] * a[13] + a[8]  * a[5] * a[14] -
+             a[8] * a[6]  * a[13] - a[12] * a[5]  * a[10] + a[12] * a[6] * a[9];
+
+    b[1]  = -a[1] * a[10] * a[15] + a[1]  * a[11] * a[14] + a[9]  * a[2] * a[15] -
+             a[9] * a[3]  * a[14] - a[13] * a[2]  * a[11] + a[13] * a[3] * a[10];
+    b[5]  =  a[0] * a[10] * a[15] - a[0]  * a[11] * a[14] - a[8]  * a[2] * a[15] +
+             a[8] * a[3]  * a[14] + a[12] * a[2]  * a[11] - a[12] * a[3] * a[10];
+    b[9]  = -a[0] * a[9]  * a[15] + a[0]  * a[11] * a[13] + a[8]  * a[1] * a[15] -
+             a[8] * a[3]  * a[13] - a[12] * a[1]  * a[11] + a[12] * a[3] * a[9];
+    b[13] =  a[0] * a[9]  * a[14] - a[0]  * a[10] * a[13] - a[8]  * a[1] * a[14] +
+             a[8] * a[2]  * a[13] + a[12] * a[1]  * a[10] - a[12] * a[2] * a[9];
+
+    b[2]  =  a[1] * a[6]  * a[15] - a[1]  * a[7]  * a[14] - a[5]  * a[2] * a[15] +
+             a[5] * a[3]  * a[14] + a[13] * a[2]  * a[7]  - a[13] * a[3] * a[6];
+    b[6]  = -a[0] * a[6]  * a[15] + a[0]  * a[7]  * a[14] + a[4]  * a[2] * a[15] -
+             a[4] * a[3]  * a[14] - a[12] * a[2]  * a[7]  + a[12] * a[3] * a[6];
+    b[10] =  a[0] * a[5]  * a[15] - a[0]  * a[7]  * a[13] - a[4]  * a[1] * a[15] +
+             a[4] * a[3]  * a[13] + a[12] * a[1]  * a[7]  - a[12] * a[3] * a[5];
+    b[14] = -a[0] * a[5]  * a[14] + a[0]  * a[6]  * a[13] + a[4]  * a[1] * a[14] -
+             a[4] * a[2]  * a[13] - a[12] * a[1]  * a[6]  + a[12] * a[2] * a[5];
+
+    b[3]  = -a[1] * a[6]  * a[11] + a[1]  * a[7]  * a[10] + a[5]  * a[2] * a[11] -
+             a[5] * a[3]  * a[10] - a[9]  * a[2]  * a[7]  + a[9]  * a[3] * a[6];
+    b[7]  =  a[0] * a[6]  * a[11] - a[0]  * a[7]  * a[10] - a[4]  * a[2] * a[11] +
+             a[4] * a[3]  * a[10] + a[8]  * a[2]  * a[7]  - a[8]  * a[3] * a[6];
+    b[11] = -a[0] * a[5]  * a[11] + a[0]  * a[7]  * a[9]  + a[4]  * a[1] * a[11] -
+             a[4] * a[3]  * a[9]  - a[8]  * a[1]  * a[7]  + a[8]  * a[3] * a[5];
+    b[15] =  a[0] * a[5]  * a[10] - a[0]  * a[6]  * a[9]  - a[4]  * a[1] * a[10] +
+             a[4] * a[2]  * a[9]  + a[8]  * a[1]  * a[6]  - a[8]  * a[2] * a[5];
+
+    // Compute the determinant
+    float det = a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12];
+
+    // Compute the inverse if the determinant is not zero
+    // (don't use an epsilon because the determinant may *really* be tiny)
+    if (det != 0.f)
+    {
+        return Transform3D(b[0] / det, b[4] / det, b[8]  / det, b[12] / det,
+                           b[1] / det, b[5] / det, b[9]  / det, b[13] / det,
+                           b[2] / det, b[6] / det, b[10] / det, b[14] / det,
+                           b[3] / det, b[7] / det, b[11] / det, b[15] / det);
+    }
+    else
+    {
+        return Transform3D(Identity);
+    }
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::combine(const TransformBase& transform)
+{
+    TransformBase::combine(transform);
+    return *this;
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::translate(const Vector3f& offset)
+{
+    Transform3D translation(1, 0, 0, offset.x,
+                            0, 1, 0, offset.y,
+                            0, 0, 1, offset.z,
+                            0, 0, 0, 1);
+
+    return combine(translation);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::rotate(float angle, const Vector3f& axis)
+{
+    float rad = angle * 3.141592654f / 180.f;
+    float cos = std::cos(rad);
+    float sin = std::sin(rad);
+
+    //~ Vector3f v = axis / std::sqrt(axis.x * axis.x +
+                                  //~ axis.y * axis.y +
+                                  //~ axis.z * axis.z);
+
+    float xx = axis.x * axis.x;
+    float xy = axis.x * axis.y;
+    float xz = axis.x * axis.z;
+    float yy = axis.y * axis.y;
+    float yz = axis.y * axis.z;
+    float zz = axis.z * axis.z;
+    float xs = axis.x * sin;
+    float ys = axis.y * sin;
+    float zs = axis.z * sin;
+    float mc = 1.0f - cos;
+
+    Transform3D rotation(xx * mc + cos, xy * mc - zs,  xz * mc + ys,  0,
+                         xy * mc + zs,  yy * mc + cos, yz * mc - xs,  0,
+                         xz * mc - ys,  yz * mc + xs,  zz * mc + cos, 0,
+                         0,             0,             0,             1);
+
+    return combine(rotation);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::rotate(float angle, const Vector3f& axis, const Vector3f& center)
+{
+    //! @todo implement "more easily (and efficiently) than":
+    return translate(-center).rotate(angle, axis).translate(center);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::scale(float factor)
+{
+    Transform3D scaling(factor, 0,      0,      0,
+                        0,      factor, 0,      0,
+                        0,      0,      factor, 0,
+                        0,      0,      0,      1);
+
+    return combine(scaling);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::scale(float factor, const Vector3f& center)
+{
+    //! @todo implement "more easily (and efficiently) than":
+    return translate(-center).scale(factor).translate(center);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::scale(const Vector3f& factors)
+{
+    Transform3D scaling(factors.x, 0,         0,         0,
+                        0,         factors.y, 0,         0,
+                        0,         0,         factors.z, 0,
+                        0,         0,         0,         1);
+
+    return combine(scaling);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& Transform3D::scale(const Vector3f& factors, const Vector3f& center)
+{
+    //! @todo implement "more easily (and efficiently) than":
+    return translate(-center).scale(factors).translate(center);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D operator *(const Transform3D& left, const Transform3D& right)
+{
+    return Transform3D(left).combine(right);
+}
+
+
+////////////////////////////////////////////////////////////
+Transform3D& operator *=(Transform3D& left, const Transform3D& right)
+{
+    return left.combine(right);
+}
+
+
+////////////////////////////////////////////////////////////
+Vector2f operator *(const Transform3D& left, const Vector2f& right)
+{
+    return left.transformPoint(right);
+}
+
+} // namespace sf
